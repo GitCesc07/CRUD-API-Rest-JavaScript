799659b0a990e398f602149aed6a107ca2eb79ed
CescDevGit07 <cesar04071997@gmail.com>
2023-04-28 22:42:07 -0600

    Mostrando listado que existe en nuestra Api, y se carga en el grid del index.html

--- js/API.js
+++ js/API.js
@@ -1,5 +1,6 @@
 const url = "http://localhost:4000/clientes";
 
+// Cuando se crea un nuevo cliente
 export const nuevoCliente = async cliente => {
   try {
     await fetch(url, {
@@ -11,6 +12,18 @@ export const nuevoCliente = async cliente => {
     });
 
     window.location.href = 'index.html';
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+// Obtiene todo los clientes que hay en nuestra API
+export const obtenerClientes = async () => {
+  try {
+    const resultado = await fetch(url)
+    const clientes = await resultado.json();
+    return clientes;
   }
   catch (error) {
     console.log(error);
